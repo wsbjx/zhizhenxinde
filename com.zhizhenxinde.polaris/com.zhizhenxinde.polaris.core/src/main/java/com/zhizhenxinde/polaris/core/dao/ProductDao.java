d264a26bb69ab30b6c15bae9bcc792a0343b28c0
wsbjx <wsbjx@163.com>
2014-10-29 00:31:29 +0800

    test

--- com.zhizhenxinde.polaris/com.zhizhenxinde.polaris.core/src/main/java/com/zhizhenxinde/polaris/core/dao/ProductDao.java
+++ com.zhizhenxinde.polaris/com.zhizhenxinde.polaris.core/src/main/java/com/zhizhenxinde/polaris/core/dao/ProductDao.java
@@ -1,5 +1,8 @@
 package com.zhizhenxinde.polaris.core.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ProductDao
 {
 
--- com.zhizhenxinde.polaris/com.zhizhenxinde.polaris.core/src/main/java/com/zhizhenxinde/polaris/core/service/ProductService.java
+++ com.zhizhenxinde.polaris/com.zhizhenxinde.polaris.core/src/main/java/com/zhizhenxinde/polaris/core/service/ProductService.java
@@ -1,5 +1,8 @@
 package com.zhizhenxinde.polaris.core.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService
 {
 

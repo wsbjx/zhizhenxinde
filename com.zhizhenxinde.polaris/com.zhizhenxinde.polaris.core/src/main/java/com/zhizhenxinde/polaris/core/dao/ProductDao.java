9dc94a5ed2b3c344c383d1048d6f7a9152ac35b1
wsbjx <wsbjx@163.com>
2014-11-03 00:18:32 +0800

    asdf

--- com.zhizhenxinde.polaris/com.zhizhenxinde.polaris.core/src/main/java/com/zhizhenxinde/polaris/core/dao/ProductDao.java
+++ com.zhizhenxinde.polaris/com.zhizhenxinde.polaris.core/src/main/java/com/zhizhenxinde/polaris/core/dao/ProductDao.java
@@ -1,9 +1,28 @@
 package com.zhizhenxinde.polaris.core.dao;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Repository;
+
+import com.zhizhenxinde.polaris.common.dao.HibernateTemplate;
+import com.zhizhenxinde.polaris.core.vo.Product;
 
 @Repository
 public class ProductDao
 {
+	@Resource
+	private HibernateTemplate hibernateTemplate;
+
+	public void add(Product product)
+	{
+		hibernateTemplate.save(product);
+	}
+
+	public void delete(String id)
+	{
+		hibernateTemplate.executeUpdate("delete from Product t where t.id=?", new Object[] { id });
+	}
+	
+	
 
 }
